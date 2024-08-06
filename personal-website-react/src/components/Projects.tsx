9495d61fa0be004cfbee3c52171bd78e313b524c
DillanMilo <dillanxx@icloud.com>
2024-08-06 15:35:17 +0200

    add projects css file and updated projects tsx file

---
+++ personal-website-react/src/components/Projects.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Projects.css"; // Ensure you create the corresponding CSS file
+
+const Projects: React.FC = () => {
+  return (
+    <div id="projects">
+      <header>
+        <a href="/" className="back-button">
+          <u>Back</u>
+        </a>
+        <h1 className="projects">Projects</h1>
+        <br />
+        <br />
+      </header>
+      <h3 className="tap">Tap on any below to view</h3>
+      <ol className="project">
+        <li>
+          <a
+            href="https://game-hub-x.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Game Hub
+          </a>
+        </li>
+        <br />
+        {/* Add more project links here */}
+      </ol>
+    </div>
+  );
+};
+
+export default Projects;
