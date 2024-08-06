6d8e55cb879be1a6ce15ebbab7be2d5902be4065
DillanMilo <dillanxx@icloud.com>
2024-08-06 15:29:07 +0200

    Add About component with styles and update App.tsx for routing"

--- personal-website-react/src/App.tsx
+++ personal-website-react/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/About";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ personal-website-react/src/components/About.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./About.css";
+
+const About: React.FC = () => {
+  return (
+    <div id="info">
+      <header>
+        <br />
+        <br />
+        <a href="/" className="back-button">
+          <u>Back</u>
+        </a>
+        <h1 className="infotitle">
+          <u>About Me</u>
+        </h1>
+      </header>
+      <section id="info-content">
+        <p>
+          Welcome to my digital space! I'm Dillan, a passionate software
+          engineer on a journey to craft innovative solutions. My fascination
+          with technology is only rivaled by my love for creativity. Whether I'm
+          immersed in coding or exploring the intersection of{" "}
+          <span className="infos">design and functionality,</span> I find joy in
+          transforming ideas into tangible digital experiences. Join me as we
+          navigate the ever-evolving landscape of technology, where every line
+          of code tells a unique story.
+        </p>
+        <h2 className="skillsh">
+          <u>Skills</u>
+        </h2>
+        <ul>
+          <li className="skills">Web Development Foundations</li>
+          <br />
+          <li className="skills">
+            Creating and Deploying Interactive Websites
+          </li>
+          <br />
+          <li className="skills">
+            Front-end Development (HTML, CSS, Javascript, React, Redux, Mocha)
+          </li>
+          <br />
+          <li className="skills">Certified in Software Engineering</li>
+          <br />
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default About;
