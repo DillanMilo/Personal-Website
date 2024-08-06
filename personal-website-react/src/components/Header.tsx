adc32115c1297119c28f7a49dac049255735adcb
DillanMilo <dillanxx@icloud.com>
2024-08-06 15:34:04 +0200

    add header css file and update tsx file

---
+++ personal-website-react/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css"; // Ensure you create the corresponding CSS file
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
