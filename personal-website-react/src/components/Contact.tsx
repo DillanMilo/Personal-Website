46259130afef412653511a77400609d852fb4054
DillanMilo <dillanxx@icloud.com>
2024-08-07 16:00:38 +0200

    Add contact information to Contact component

--- personal-website-react/src/components/Contact.tsx
+++ personal-website-react/src/components/Contact.tsx
@@ -1,26 +1,81 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Global.css";
+
+// Import images
+import twitterIcon from "../Images/x-twitter-copy.svg";
+import githubIcon from "/Users/dillanmilosevich/Personal-Website/personal-website-react/src/Images/github-3-copy.svg";
+import linkedinIcon from "/Users/dillanmilosevich/Personal-Website/personal-website-react/src/Images/linkedin-copy.svg";
 
 const Contact: React.FC = () => {
   return (
-    <div id="contact">
+    <div>
       <header>
-        <a href="/" className="back-button">
+        <Link to="/" className="back-button">
           <u>Back</u>
-        </a>
-        <h1 className="heading">Contact Me</h1>
+        </Link>
+        <h1 className="heading">Contact</h1>
       </header>
-      <section className="contacts">
-        <ul>
-          <li>
-            <a href="mailto:dillanxx@icloud.com">dillanxx@icloud.com</a>
-          </li>
-          <li>
-            <a href="tel:+0820663459">+082 0663459</a>
-          </li>
-          {/* Add more contact info if needed */}
-        </ul>
-      </section>
+      <br />
+      <body>
+        <p className="outro">
+          Let's turn your ideas into digital masterpieces! Whether it's coding
+          magic or turning coffee into code, I'm here for it.
+          <span className="infos"> Reach out,</span> and let's create something
+          extraordinary together!
+        </p>
+        <br />
+        <br />
+        <div className="contacts">
+          <ul>
+            <li>
+              <strong>Email me:</strong>{" "}
+              <a href="mailto:Dillanxx@icloud.com">Dillanxx@icloud.com</a>
+            </li>
+            <br />
+            <li>
+              <strong>Shoot me a DM on </strong>
+              <a
+                href="https://twitter.com/dillanx1x"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={twitterIcon}
+                  alt="Twitter"
+                  style={{ marginLeft: "10px" }}
+                />
+              </a>
+            </li>
+            <br />
+            <br />
+            <li>
+              Come over to
+              <a
+                href="https://github.com/dashboard"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+            </li>
+            <br />
+            <br />
+            <li>
+              Say "hey" on
+              <a
+                href="https://www.linkedin.com/feed/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedinIcon} alt="LinkedIn" />
+              </a>
+            </li>
+            <br />
+            <br />
+          </ul>
+        </div>
+      </body>
     </div>
   );
 };
